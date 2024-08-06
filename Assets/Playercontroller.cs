828f4e51f8de2adca52b41aa428c71645a8ce138
Lenisama <minhlh22@gmail.com>
2024-08-06 20:56:51 +0700

    Project files

---
+++ Assets/Playercontroller.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Playercontroller : MonoBehaviour
+{
+    Vector2 moveInput;
+    public float walkSpeed = 5f;
+    Rigidbody2D rb;
+    Animator animator;
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool IsMoving { get
+    {
+        return _isMoving;
+    } private set
+    {
+        _isMoving = value;
+        animator.SetBool("isMoving", value);
+    }}
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+    }
+    private void FixUpdate()
+    {
+
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        //moveInput = context.ReadValue<Vector2>();
+        IsMoving = moveInput != Vector2.zero;
+    }
+}
