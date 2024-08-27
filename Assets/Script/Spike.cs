4bf756d773f1418b09315a751ee0ac23433d1dc3
Lenisama <minhlh22@gmail.com>
2024-08-27 20:59:30 +0700

    Use coroutine

--- Assets/Script/Spike.cs
+++ Assets/Script/Spike.cs
@@ -5,6 +5,7 @@ using UnityEngine.Animations;
 
 public class Spike : MonoBehaviour
 {
+    public float savedTime;
     public float speed = 50;
     // Start is called before the first frame update
     public void RiseUp()
@@ -15,4 +16,14 @@ public class Spike : MonoBehaviour
     {
             transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime);
     }
+    public IEnumerable Rise()
+    {
+         int i = 0;
+        while(i < 24)
+        {
+        RiseUp();
+        yield return new WaitForSeconds(1);
+        }
+        FallDown();
+    }  
 }
